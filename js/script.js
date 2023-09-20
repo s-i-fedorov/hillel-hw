711f718536b0d557bc129d2ba1b58540e1ea3a02
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-09-21 00:52:34 +0300

    hw 3 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+let firstOperand = Number(prompt('Write the first operand',));
+let secondOperand = Number(prompt('Write the second operand',));
+let c = firstOperand+secondOperand;
+let result = 'Результат:'+firstOperand+'+'+secondOperand+'='+c;
+
+console.log(firstOperand);
+console.log(secondOperand);
+console.log(result);
