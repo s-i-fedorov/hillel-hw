ba9ce61044a7fa3b792189ee7db79d7d58f4ef6b
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-09-21 00:24:12 +0300

    hw 2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Task #1
+let name = 'John';
+let admin = name;
+alert(admin);
+
+// Task #2
+let ourPlanet = 'Earth';
+let currentUser = 'currentUser';
+
+// Task #3
+let a = "10" + 10;
+console.log(typeof a);
