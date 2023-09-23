ae07a2da1a2e7476c056963b32b69ff3fa36b1bf
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-09-24 00:29:20 +0300

    hw 4 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+let choise = prompt('Please, choise one option: add, sub, mult or div',)
+let firstOperand = prompt('Write the first operand',);
+let secondOperand = prompt('Write the second operand',);
+
+if (choise=='add'){
+    console.log(firstOperand+'+'+secondOperand+'='+(Number(firstOperand)+Number(secondOperand)))};
+if (choise=='sub'){
+    console.log(firstOperand+'-'+secondOperand+'='+(Number(firstOperand)-Number(secondOperand)))};
+if (choise=='mult'){
+    console.log(firstOperand+'*'+secondOperand+'='+(Number(firstOperand)*Number(secondOperand)))};
+if (choise=='div'){
+    console.log(firstOperand+'/'+secondOperand+'='+(Number(firstOperand)/Number(secondOperand)))};
+
+console.log(firstOperand);
+console.log(secondOperand);
+console.log(choise);
