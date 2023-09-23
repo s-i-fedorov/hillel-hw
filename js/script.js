f0d53adb74a671b788853e326aa9b3b33633c978
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-09-24 00:31:39 +0300

    hw 4 completed

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ let firstOperand = prompt('Write the first operand',);
 let secondOperand = prompt('Write the second operand',);
 
 if (choise=='add'){
-    console.log(firstOperand+'+'+secondOperand+'='+(Number(firstOperand)+Number(secondOperand)))};
+    console.log(firstOperand+'+'+secondOperand+'='+(Number(firstOperand)+Number(secondOperand)))}
 if (choise=='sub'){
-    console.log(firstOperand+'-'+secondOperand+'='+(Number(firstOperand)-Number(secondOperand)))};
+    console.log(firstOperand+'-'+secondOperand+'='+(Number(firstOperand)-Number(secondOperand)))}
 if (choise=='mult'){
-    console.log(firstOperand+'*'+secondOperand+'='+(Number(firstOperand)*Number(secondOperand)))};
+    console.log(firstOperand+'*'+secondOperand+'='+(Number(firstOperand)*Number(secondOperand)))}
 if (choise=='div'){
-    console.log(firstOperand+'/'+secondOperand+'='+(Number(firstOperand)/Number(secondOperand)))};
+    console.log(firstOperand+'/'+secondOperand+'='+(Number(firstOperand)/Number(secondOperand)))}
 
 console.log(firstOperand);
 console.log(secondOperand);
