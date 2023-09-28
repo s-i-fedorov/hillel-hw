93675a74fbbcb7913f487003eab7cc331963aa11
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-09-29 00:51:33 +0300

    hw 8 in the process

--- js/script.js
+++ js/script.js
@@ -12,11 +12,19 @@
 // console.log(object.name);
 
 // Task #2
-const user = {
-    name: "John"
-};
-// Так, це працюватиме. Тому що константою є об'єкт, об'єкт змінювати не можна.
-// А властивості можна змінювати і додавати
-user.name = "Pete";
-user.surname = "Culkin";
-console.log(user.name, user.surname);
+// const user = {
+//     name: "John"
+// };
+// // Так, це працюватиме. Тому що константою є об'єкт, об'єкт змінювати не можна.
+// // А властивості можна змінювати і додавати
+// user.name = "Pete";
+// user.surname = "Culkin";
+// console.log(user.name, user.surname);
+
+// Task #3
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+let sum =
