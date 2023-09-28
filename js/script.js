d529b35371a2432db951f9b90f9769f949f256e4
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-09-29 00:30:32 +0300

    hw 8 in the process

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+// Task #1
+// let object = {
+//
+// };
+// object.name = 'John';
+// object.surname = 'Smith';
+// object.name = 'Pete';
+// delete object.name;
+//
+// console.log(object);
+// console.log(object.name);
+
+// Task #2
+const user = {
+    name: "John"
+};
+
+// Так, це працюватиме. Тому що константою є об'єкт, об'єкт змінювати не можна.
+А властивості можна змінювати і додавати
+
+user.name = "Pete";
