d53576f792e42145d705921e614d04d184d92d04
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-09-30 08:35:47 +0300

    hw 8 completed

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 'use strict';
 // Task #1
-// let object = {
-//
-// };
-// object.name = 'John';
-// object.surname = 'Smith';
-// object.name = 'Pete';
-// delete object.name;
-//
-// console.log(object);
-// console.log(object.name);
+let object = {
+
+};
+object.name = 'John';
+object.surname = 'Smith';
+object.name = 'Pete';
+delete object.name;
+
+console.log(object);
+console.log(object.name);
 
 // Task #2
-// const user = {
-//     name: "John"
-// };
-// // Так, це працюватиме. Тому що константою є об'єкт, об'єкт змінювати не можна.
-// // А властивості можна змінювати і додавати
-// user.name = "Pete";
-// user.surname = "Culkin";
-// console.log(user.name, user.surname);
+const user = {
+    name: "John"
+};
+
+// Так, це працюватиме. Тому що константою є сам об'єкт, об'єкт змінювати не можна.
+// Посилковий тип змінних. Властивості які він зберігає це посилання.
+// Властивості можна змінювати і додавати.
+
+user.name = "Pete";
+user.surname = "Culkin";
+console.log(user.name, user.surname);
 
 // Task #3
 let salaries = {
