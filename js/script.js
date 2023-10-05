000ec2c6a18f31dc26f205154018fdb9cf2ea70d
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-10-06 01:54:49 +0300

    hw_14 commit 1

--- js/script.js
+++ js/script.js
@@ -13,18 +13,23 @@
 //     } else {
 //         console.log('OK!')
 //     }
+
 let numOrStr = prompt('input number or string');
     console.log(numOrStr)
 switch (numOrStr) {
     case null:
         console.log('ви скасували');
         break;
-    case '':
+    case (''):
         console.log('Empty String');
         break;
-    case isNaN(+numOrStr):
+    case (isNaN(+numOrStr)):
         console.log('number is Ba_NaN')
         break;
     default:
         console.log('OK!');
 }
+
+
+// let test = prompt('input value');
+// console.log(+test)
