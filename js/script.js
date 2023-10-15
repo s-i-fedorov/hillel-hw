c61a2a1b726ef4a62bfccb0d51dc9b4ab93eda7c
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-10-15 11:48:52 +0300

    hw_21 first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+let a = prompt('Please, enter a number');
+if (a === null){
+    alert('ви не ввели число')
+} else if (isNaN(+a)){
+    alert('ви ввели некоректне число')
+} else if (a === ''){
+    alert('пустий рядок')
+}
+let b = prompt('Please, enter power of a number ');
+
+let result = null;
+function calcPower(){
+    result = a ** b;
+    console.log(result);
+}
+calcPower();
