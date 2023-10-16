18e73726bd3316938b52587c249b393be50fbba9
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-10-17 00:28:45 +0300

    hw_22 completed

--- js/script.js
+++ js/script.js
@@ -1,35 +1,20 @@
 'use strict';
-// Вам потрібно написати реалізацію функції padString, яка приймає 4 аргументи:
-// рядок
-// число, яке є довгим рядок, який ми хочемо отримати в результаті виконання функції
-// символ (рядок довжиною 1 символ) — яким доповниться рядок, якщо це буде потрібно
-// параметр булеан (true або false), який визначає, додавати символи ліворуч або праворуч
-// (за замовчуванням праворуч)
-//
-// Обов'язково при написанні функції необхідно перевірити аргументи, які ми передали, і якщо якихось ' +
-// 'аргументів немає, то повернути рядок з помилкою з функції (return 'some error'). Повідомлення з ' +
-// 'помилкою має бути різним залежно від тієї умови, за якою не пройшла перевірка.
-// Результат виклику функції потрібно вивести в консоль після завершення функції.
-//     Наприклад:
-// Виклик padString('hello', 8, '*') поверне рядок hello***
-// А виклик padString('hello', 6, '*', false) поверне рядок *hello
-// Виклик padString('hello', 2) поверне 'he' — якщо число менше розміру введеного рядка, потрібно рядок
-// обрізати за допомогою методу substr
 
 function padString(row,rowLength,superSymbol,leftRight = true){
-    let result = '';
     if (row === undefined){
         return console.log('ви не ввели рядок')
     } else if (rowLength === undefined) {
         return console.log('ви не ввели довжину рядка')
-    } else if (superSymbol === undefined) {
+    } else if ((superSymbol === undefined) && (row.length<=rowLength)) {
         return console.log('ви не ввели спецсимвол')
     }
-    if (leftRight === true){
-        console.log(row.padStart(rowLength,superSymbol))
-    } else {
+    if (rowLength <= row.length) {
+        console.log(row.substring(0,rowLength))
+    }else if(leftRight === true) {
         console.log(row.padEnd(rowLength,superSymbol))
+    } else {
+        console.log(row.padStart(rowLength,superSymbol))
     }
     }
 
-    padString(helloWorld, )
+    padString('helloWorld',15,'-',false )
