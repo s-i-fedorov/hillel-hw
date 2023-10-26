6a9de5400ca767c7d31ab6145ba98b3f12bdf500
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-10-27 00:09:15 +0300

    hw_25 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let foo = prompt('Введіть елементи масиву через кому');
+let arrOne = foo.split(',')
+alert(`Ви ввели масив:\n [${arrOne}]`);
+
+let arrTwo = arrOne.sort((a,b)=>a-b);
+alert(`Масив відсортовано за зростанням:\n [${arrTwo}]`);
+
+arrTwo.splice(1,3)
+
+alert(`З масиву видалені елементи з 2 по 4 включно:\n [${arrTwo}]`);
