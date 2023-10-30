184575e0dbcbf46d55436ec902a4b342d8a085a3
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-10-30 22:12:36 +0200

    hw_28 2 commit

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@
 // indexOf,lastIndexOf,find,findIndex,includes,every та some,
 //     вони працюють так само, як одноіменні методи масивів.
 
-const arrOne = [1,2,3,4,5,6];
-
-const callback = (element, index, arr) => {}
+const arrOne = [1,2,3,4,5,18];
+const arrString = ['apple', 'banana', 'orange', 'chery', 'peach'];
 
 const indexOf = (arr, item, from = 0) => {
     if (from>=arr.length) return console.log(-1);
@@ -17,6 +16,7 @@ const indexOf = (arr, item, from = 0) => {
     }
     return console.log(-1);
 }
+console.log('-----------indexOf:');
 indexOf(arrOne,6,-0)
 
 const lastIndexOf = (arr, item, from = arr.length) => {
@@ -27,12 +27,31 @@ const lastIndexOf = (arr, item, from = arr.length) => {
     }
     return console.log(-1);
 }
+console.log('-----------lastIndexOf:');
 lastIndexOf(arrOne,5,-2)
 
+//function find
+const find = (arr, callback) => {
+    for(let i = 0; i<=arr.length;i++){
+        // let element = callback(arr[i]);
+        let element = arr[i];
+        if(callback(element)) return console.log(arr[i])
+    }
+    return console.log('undefined');
+};
+console.log('-----------find:')
+find(arrOne,item=>item>6);
+find(arrString,item=>item.length>5)
 
-const find = () => {}
-
-const findIndex = () => {}
+const findIndex = (arr, callback) => {
+    for(let i = 0; i<=arr.length;i++){
+        let element = arr[i];
+        if(callback(element)) return console.log(i)
+    }
+    return console.log(-1);
+};
+console.log('-----------findIndex:');
+findIndex(arrString,item=>item==='peach')
 
 const includes = (arr, item, from = 0) => {
     if (from>=arr.length) return console.log(false);
@@ -42,11 +61,26 @@ const includes = (arr, item, from = 0) => {
     }
     return console.log(false);
 }
+console.log('-----------includes:')
 includes(arrOne, 1,5)
 
-const every = () => {}
-
-const some = (arr, callback) => {
-
+const every = (arr, callback) => {
+    for(let i = 0; i<=arr.length;i++){
+        let element = callback(arr[i]);
+        if(element) return console.log(true)
+    }
+    return console.log(false);
 }
-some(arrOne,element=>element>10)
+console.log('-----------every:');
+every(arrString,item=>item.length<2);
+
+//function some
+const some = (arr, callback) => {
+for(let i = 0; i<=arr.length;i++){
+    let element = callback(arr[i]);
+    if(element) return console.log(true)
+}
+    return console.log(false);
+}
+console.log('-----------some:')
+some(arrString,item=>item==='asdf')
