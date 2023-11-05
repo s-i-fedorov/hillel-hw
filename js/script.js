f0dddb26d6a29715314525b0e4c4d7a125fc074b
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-06 01:09:14 +0200

    hw_30 commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+(function (){
+
+    const arr = [
+        1, 2, 3, 4, {},
+        [1, 2, 3],
+        [1, 2, 3],
+        [3, 4,
+            [5, 6, 7]
+        ]
+    ]
+
+
+
+    function outerFlat() {
+        let res = []
+        return function flat(arr) {
+            for(let i = 0; i < arr.length; i++) {
+                if(Array.isArray(arr[i])) {
+                    flat(arr[i], res)
+                } else {
+                    res.push(arr[i])
+                }
+            }
+            return res;
+        }
+    }
+
+    const flat = outerFlat()
+    console.log(flat(arr));
+    console.log(flat(arr));
+
+})()
