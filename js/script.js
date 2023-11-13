d50880b5ba338b372d791ca8983fd8a40e9d57c4
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-13 23:28:30 +0200

    hw 32 play with bind

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 'use strict';
 // Написати свою реалізацію функції bind
 // При реалізації функції bind заборонено використовувати методи call та apply
+function example(){
+    console.log(this.name)
+}
+
+const a = {
+    name: 'Serhii',
+    age: 36,
+}
+const b = {
+    name: 'Vika',
+    age: 29
+}
+const res = example.bind(a)
+res()
+
+// console.log( example.bind(a))
