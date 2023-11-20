4fc65949118c60a4054876ce12cd777f24ade118
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-20 23:33:48 +0200

    hw_36 first commit 1

--- js/solution.js
+++ js/solution.js
@@ -1,4 +1,19 @@
 'use strict';
 (function (){
 
+    let bodyEl = document.body
+    let li = bodyEl.childNodes[1].children
+    let arr = []
+    console.log('----------Task 1')
+    for (let liElement of li) {
+        console.log(liElement)
+    }
+    console.log('----------Task 2')
+    console.log('Кількість елементів у списку - ' + li.length)
+    console.log('----------Task 3')
+    for (let liElement of li) {
+        arr.push(liElement.outerText)
+    }
+    console.log(arr)
+
 })()
