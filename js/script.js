123c3325f9fdb99d0d6af56891d97c8816a4bf7a
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-21 15:39:48 +0200

    hw_37 commit

--- js/script.js
+++ js/script.js
@@ -3,14 +3,9 @@
 // Написать функцию generateList(array), которая принимает массив из чисел и массивов чисел
 // (например [1,2,3]) и генерирует список из элементов:
 //
-//     <ul>
-//         <li>1</li>
-//         <li>2</li>
-//         <li>3</li>
-//     </ul>
 // А если в массиве встречается массив (например, [1,2, [1.1,1.2,1.3] ,3]) то делать вложенный список.
 //     Для проверки на массив используйте Array.isArray()
-//
+
 // <ul>
 // <li>1</li>
 // <li>2</li>
@@ -26,6 +21,13 @@
 
 (function (){
 
+let ul = document.createElement('ul')
+    let li = document.createElement('li')
 
+    document.body.append(ul)
+    console.log(document.querySelector(ul))
+
+    // console.log(document.body)
+    console.log(ul)
 
 })()
