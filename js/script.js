24f5e146dc076ab082a6cb8b259144ecd2ca8399
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-23 01:58:16 +0200

    hw_38 commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@
 // P.S. Подумайте о том как лучше обновлять DOM структуру, от этого зависит производительность скрипта.
 (function (){
 
+let tab = document.createElement('table')
+    let tr = document.createElement('tr')
+    let td = document.createElement('td')
+    document.body.append(tab)
+    for(let i=0;i<10;i++){
+        tab.append(tr)
+        for(let a=0;a<10;a++){
+            tr.append(td)
+            let qS = document.querySelectorAll('td')
+            let lTd = qS[qS.length-1]
+            lTd.innerHTML=i+10+a;
 
+        }
+
+    }
 
 })()
