c85a3099e1467deea9ea4af62ab2d761f05ab4fc
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-23 00:48:31 +0200

    hw_32 completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-'use strict';
+// 'use strict';
 // Написати свою реалізацію функції bind
 // При реалізації функції bind заборонено використовувати методи call та apply
+
+(function (){
+
 function showInfo(){
     console.log(this.name+' '+this.age)
 }
-function multToNum(a,b){
-    let x = a;
-    let y = b;
-    return x*y;
-}
+
 
 const a = {
     name: 'Serhii',
@@ -18,25 +17,16 @@ const b = {
     name: 'Vika',
     age: 29
 }
-const c = [1,2,3,4,5]
 
 function myBind(addFunc, addObj){
-    if (addObj===null) {
-        // console.log(args)
-        return addFunc()
-    } else {
+
         addObj.f = addFunc;
-        let result = addObj.f(args);
+        let result = addObj.f();
         delete addObj.f;
         return result;
+
     }
-    }
 
-// myBind(showInfo,a)
-const double = multToNum(3)
-// const double = myBind(multToNum,null,3)
-console.log(double(2))
+myBind(showInfo,b)
 
-// let exemp
-// console.log(typeof exemp)
-
+})()
