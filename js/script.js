8d149940c3039263a6c056d35ad21ad5c09364c5
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-24 15:46:56 +0200

    hw_38 completed

--- js/script.js
+++ js/script.js
@@ -4,21 +4,15 @@
 (function (){
 
 let tab = document.createElement('table')
-    // let tr = document.createElement('tr')
-    // let td = document.createElement('td')
     document.body.append(tab)
     for(let i=0;i<10;i++){
         let tr = document.createElement('tr')
         tab.append(tr)
         for(let a=0;a<10;a++){
-            // tr.append(td)
-            // let lTd = document.querySelector('td:last-of-type')
             let td = document.createElement('td')
-            td.textContent=i*10+a+1;
+            td.innerHTML=i*10+a+1;
             tr.append(td)
-
         }
-
     }
 
 })()
