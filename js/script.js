ff6621273584b9570679e2b115e89cb83dbbb6d7
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-24 15:49:20 +0200

    hw_38 completed

--- js/script.js
+++ js/script.js
@@ -3,16 +3,19 @@
 // P.S. Подумайте о том как лучше обновлять DOM структуру, от этого зависит производительность скрипта.
 (function (){
 
-let tab = document.createElement('table')
-    document.body.append(tab)
-    for(let i=0;i<10;i++){
-        let tr = document.createElement('tr')
-        tab.append(tr)
-        for(let a=0;a<10;a++){
-            let td = document.createElement('td')
-            td.innerHTML=i*10+a+1;
-            tr.append(td)
+    function createTable() {
+        let tab = document.createElement('table')
+        document.body.append(tab)
+        for (let i = 0; i < 10; i++) {
+            let tr = document.createElement('tr')
+            tab.append(tr)
+            for (let a = 0; a < 10; a++) {
+                let td = document.createElement('td')
+                td.innerHTML = i * 10 + a + 1;
+                tr.append(td)
+            }
         }
     }
+    createTable()
 
 })()
