2f12ef5685fdcc39b507b2f131858e6c15cfccf9
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-25 23:32:48 +0200

    hw_39 add event

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 'use strict';
 // Є текстове поле на сторінці. Під час фокусування на цьому полі збоку з'являється <div>.
-// При пропажі фокусу - <div> так само пропадає
+// При пропажі фокуса - <div> так само пропадає
 (function (){
 
     // mouseover / mouseout
 const ghostDiv = document.createElement('div')
     ghostDiv.setAttribute('style','width: 100px; height: 100px; background-color: green;')
+    // ghostDiv.setAttribute('data-ghost')
     document.body.append(ghostDiv)
     console.log(ghostDiv)
+    const event = document.querySelector('[data-form]')
+    event.addEventListener('mouseover', ()=>{
+        console.log('it works')
+    })
 })()
