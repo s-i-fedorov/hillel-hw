4056e686bd488334997ef1ad77184359bb6fe898
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-11-29 01:00:44 +0200

    hw_41 task2 completed

--- js/script.js
+++ js/script.js
@@ -8,35 +8,43 @@
     //     Завдання: Створіть систему вкладок (tabs), де клік по вкладці показує відповідний контент, а інші
     // вкладки стають прихованими. Технології: Використання JavaScript для переключення між вкладками і
     // управління видимістю контенту.
-    const tab1 = document.querySelector('.tab1')
-    const tab2 = document.querySelector('.tab2')
-    const tab3 = document.querySelector('.tab3')
-    const content1 = document.querySelector('.content1')
-    const content2 = document.querySelector('.content2')
-    const content3 = document.querySelector('.content3')
-    tab1.addEventListener('click',()=>{
-        tab1.style.backgroundColor='lightgray'
-        tab2.style.backgroundColor='white'
-        tab3.style.backgroundColor='white'
-        content1.style.zIndex='1'
-        content2.style.zIndex='0'
-        content3.style.zIndex='0'
-    })
-    tab2.addEventListener('click',()=>{
-        tab1.style.backgroundColor='white'
-        tab2.style.backgroundColor='lightgray'
-        tab3.style.backgroundColor='white'
-        content1.style.zIndex='0'
-        content2.style.zIndex='1'
-        content3.style.zIndex='0'
-    })
-    tab3.addEventListener('click',()=>{
-        tab1.style.backgroundColor='white'
-        tab2.style.backgroundColor='white'
-        tab3.style.backgroundColor='lightgray'
-        content1.style.zIndex='0'
-        content2.style.zIndex='0'
-        content3.style.zIndex='1'
-    })
+    // const tab1 = document.querySelector('.tab1')
+    // const tab2 = document.querySelector('.tab2')
+    // const tab3 = document.querySelector('.tab3')
+    const row = document.querySelector('.row')
+    // const content1 = document.querySelector('.content1')
+    // const content2 = document.querySelector('.content2')
+    // const content3 = document.querySelector('.content3')
+    // tab1.addEventListener('click',(ev)=>{
+    //     tab1.style.backgroundColor='lightgray'
+    //     tab2.style.backgroundColor='white'
+    //     tab3.style.backgroundColor='white'
+    //     content1.style.zIndex='1'
+    //     content2.style.zIndex='0'
+    //     content3.style.zIndex='0'
+    //     // console.log(ev.target);
+    // })
+    // tab2.addEventListener('click',()=>{
+    //     tab1.style.backgroundColor='white'
+    //     tab2.style.backgroundColor='lightgray'
+    //     tab3.style.backgroundColor='white'
+    //     content1.style.zIndex='0'
+    //     content2.style.zIndex='1'
+    //     content3.style.zIndex='0'
+    // })
+    // tab3.addEventListener('click',()=>{
+    //     tab1.style.backgroundColor='white'
+    //     tab2.style.backgroundColor='white'
+    //     tab3.style.backgroundColor='lightgray'
+    //     content1.style.zIndex='0'
+    //     content2.style.zIndex='0'
+    //     content3.style.zIndex='1'
+    // })
+    row.addEventListener('click',(ev)=>{
+        const target = ev.target
+        target.style.backgroundColor = 'lightgray'
 
+        console.log(ev.target);
+    })
+    // console.log(row)
 })()
