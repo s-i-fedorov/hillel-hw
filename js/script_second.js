11141d05625f297c9c018e7738008afb3c5c913f
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-12-04 15:18:50 +0200

    hw_42 created ul

--- js/script_second.js
+++ js/script_second.js
@@ -4,16 +4,11 @@
     const createList = (data)=>{
         const ul = document.createElement("ul")
         for (const dataKey in data) {
-            console.log(dataKey )
-        }
-        for (const dataKey in data) {
             const li = document.createElement('li')
             li.innerHTML= dataKey+': '+data[dataKey]
             ul.appendChild(li)
         }
-
         document.body.append(ul)
-        console.log()
     }
 
     // createList()
