2ba9f9e11b81a805fc17889066a23d41b2c5e47a
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-12-04 08:44:18 +0200

    hw_42 created ul

--- js/script_second.js
+++ js/script_second.js
@@ -3,21 +3,27 @@
 
     const createList = (data)=>{
         const ul = document.createElement("ul")
-        const li = document.createElement('li')
+        for (const dataKey in data) {
+            console.log(dataKey )
+        }
+        for (const dataKey in data) {
+            const li = document.createElement('li')
+            li.innerHTML= dataKey+': '+data[dataKey]
+            ul.appendChild(li)
+        }
 
-        ul.appendChild(li)
         document.body.append(ul)
         console.log()
     }
 
-    createList()
+    // createList()
 
     const loadData=()=>{
         let data = localStorage.getItem('dataForm')
         if(data===null) return console.log('Data does not exist')
         data = JSON.parse(data)
-        // createList(data)
-        console.log(data)
+        createList(data)
+        // console.log(data)
     }
 
     document.addEventListener('DOMContentLoaded', loadData)
