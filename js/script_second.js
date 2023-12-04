25c41a5d3064c97da596714d0dc86dd699b4486e
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-12-04 23:20:04 +0200

    hw_42 completed

--- js/script_second.js
+++ js/script_second.js
@@ -11,14 +11,12 @@
         document.body.append(ul)
     }
 
-    // createList()
 
     const loadData=()=>{
         let data = localStorage.getItem('dataForm')
         if(data===null) return console.log('Data does not exist')
         data = JSON.parse(data)
         createList(data)
-        // console.log(data)
     }
 
     document.addEventListener('DOMContentLoaded', loadData)
