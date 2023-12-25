a7c0f96b895e802634f735b5930dc5c4befabeba
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-12-26 00:29:30 +0200

    hw_53 task 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
 'use strict';
 (function (){
 
+    const user = {
+        name: 'Serhii',
+        surname: 'Fedorov'
+
+    }
+
+    Object.defineProperty(user,'name',{
+        writable: false
+    });
+
+    // console.log(Object.getOwnPropertyDescriptor(user, 'name'));
+
+    Object.defineProperty(user, 'age', {
+        value: 36,
+        writable: true,
+        enumerable: false
+
+    })
+
+    // console.log(Object.getOwnPropertyDescriptor(user, 'age'));
+
+    console.log(Object.keys(user));
+
+    for (const userKey in user) {
+        console.log(userKey + ': ' + user[userKey])
+    }
+
+
+    console.log('--------Task 1.1 -------')
+
+
+
+    console.log('--------addition task -------')
+
+
 })()
