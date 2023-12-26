7685051fc9651cfa977aeddd1717e913b0f9bd25
s-i-fedorov <s.i.fedorov.87@gmail.com>
2023-12-26 09:19:50 +0200

    hw_53 completed

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@
 
     const user = {
         name: 'Serhii',
-        surname: 'Fedorov'
-
+        surname: 'Fedorov',
+        get fullName(){
+            return this.name+' '+this.surname
+        },
+        set fullName(string){
+            [this.name,this.surname] = string.split(' ');
+        }
     }
 
     Object.defineProperty(user,'name',{
@@ -15,9 +20,7 @@
 
     Object.defineProperty(user, 'age', {
         value: 36,
-        writable: true,
-        enumerable: false
-
+        writable: true
     })
 
     // console.log(Object.getOwnPropertyDescriptor(user, 'age'));
@@ -31,9 +34,32 @@
 
     console.log('--------Task 1.1 -------')
 
+    console.log('Demo getter:  '+user.fullName);
+
+    Object.defineProperty(user,'name',{
+        writable: true
+    });
+
+    user.fullName='Till Lindemann';
+    console.log('Demo setter:  '+user.fullName);
 
 
     console.log('--------addition task -------')
 
+    console.log(Object.getOwnPropertyDescriptor(user, 'name'));
+
+    Object.freeze(user);
+
+    Object.defineProperty(user,'name',{
+        writable: false
+    });
+
+    Object.defineProperty(user,'name',{
+        writable: true
+    });
+
+    console.log(Object.getOwnPropertyDescriptor(user, 'name'));
+    // user.name = 'Anton';
+    // console.log(user.name)
 
 })()
