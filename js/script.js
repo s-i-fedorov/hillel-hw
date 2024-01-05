b4ff592360633053042631e7794d39a40dd9f8aa
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-01-05 22:19:11 +0200

    hw_54 1 task

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 'use strict';
 (function (){
 
+    const example = [1,2,3,4,5]
+
+    function iterFunc (arr) {
+        let counter = arr.length;
+        function next (){
+            counter--;
+            while (counter>=0) {
+                return arr[counter]
+            }
+        }
+        return next
+    }
+
+    const foo = iterFunc(example);
+    console.log(foo())
+    console.log(foo())
+    console.log(foo())
+    console.log(foo())
+    console.log(foo())
+    console.log(foo())
+
+
 })()
