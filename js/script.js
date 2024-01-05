6dc315db12a3167876b68240fafdb7893c72e154
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-01-06 00:35:00 +0200

    hw_54 done

--- js/script.js
+++ js/script.js
@@ -8,4 +8,40 @@
 
     console.log(iterator.next())
 
+    console.log('-------task_2-------')
+
+    function* fibNumbers(){
+        let a = 0;
+        let b = 1;
+        for(let i = 3; ; i++){
+            let c=a+b;
+            a=b;
+            b=c;
+            yield b;
+        }
+    }
+
+    const fibonacci = fibNumbers()
+    console.log(fibonacci.next())
+    console.log(fibonacci.next())
+    console.log(fibonacci.next())
+    console.log(fibonacci.next())
+    console.log(fibonacci.next())
+
+    console.log('-------task_3-------')
+
+    const obj = {
+        name: 'Username',
+        surname: 'UserSurname',
+        age: 'userAge',
+        city: 'userCity',
+        born: 'userBorn',
+        car: 'userCar'
+    }
+
+    let itr = Object.keys(obj)[Symbol.iterator]()
+    console.log(itr.next())
+    console.log(itr.next())
+    console.log(itr.next())
+
 })()
