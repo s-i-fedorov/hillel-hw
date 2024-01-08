bbb24839ea7d951e2af7ebe901fb7e4f47fe8ba6
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-01-08 23:07:25 +0200

    hw_56 completed

--- js/script.js
+++ js/script.js
@@ -7,8 +7,6 @@
         this.marks = marks;
         this.visiting = Array(25).fill(null)
     }
-    let studentVovan = new Student('Vovan','Shaitan',1997,[100,90,80,80,90,100]);
-    let studentSerhii= new Student('Serhii','Fedorov', 1987,[80,100,100,100,100,90,100,80]);
 
     Student.prototype.age = function () {
         let today = new Date().getFullYear();
@@ -17,7 +15,6 @@
     Student.prototype.summary = function() {
         let avrMark = this.marks.reduce((acc, i) => acc + i, 0) / this.marks.length;
         let avrVisiting = this.visiting.reduce((acc, i) => acc + i, 0) / this.visiting.length;
-        console.log([avrMark,avrVisiting])
         if(avrMark>90 && avrVisiting>0.9) {
             return 'Молодець!'
         } else if(avrMark<90 && avrVisiting<0.9) {
@@ -35,23 +32,36 @@
         if (i===-1) return;
         this.visiting[i]=1;
     }
+    Student.prototype.allInfo = function (){
+        const allInfo = {
+            user: this.name,
+            age: this.age(),
+            visiting: this.visiting,
+            summary: this.summary(),
+        }
+        console.log(allInfo)
+    }
 
-//---------
+//---------create different users
+    let studentVovan = new Student('Vovan','Shaitan',1997,[100,100,80,80,90,100]);
+    let studentSerhii= new Student('Serhii','Fedorov', 1987,[80,100,100,100,100,90,100,80]);
+    let studentVasia= new Student('Vasia','Coco', 2005,[80,10,10,100,100,90,100,80]);
+
+    //this loops fill arrays 'visiting'
+
     for (let i = 0; i<3;i++){
         studentSerhii.absent()
     }
+    for (let i = 0; i<23;i++){
+        studentVasia.absent()
+    }
     for (let i = 0; i<25;i++){
         studentSerhii.present()
+        studentVovan.present()
     }
 
-    console.log(studentSerhii.visiting);
-    console.log(studentSerhii.summary());
-
-    // console.log(studentSerhii)
-    // console.log(studentVovan.age());
-
-    // let arr = [100,100,80,70,80,90,100,80]
-    // let avr = arr.findIndex(item=>item===80);
-    // console.log(avr)
+    studentSerhii.allInfo()
+    studentVovan.allInfo()
+    studentVasia.allInfo()
 
 })()
