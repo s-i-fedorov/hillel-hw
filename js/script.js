8f4a88e6f6efdfe276614990501314636da46ea9
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-01-08 12:31:56 +0200

    hw_55 task 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 'use strict';
 (function (){
-    function Human(name,age){
+    function Person(name,age){
         this.name = name;
         this.age = age;
     }
-    let user = new Human('Vovan',26);
-    let user2 = new Human('Serhii',36);
-    let userYoung = new Human('Valera',17);
-    Human.prototype.userInfo = function (){
+    let user = new Person('Vovan',26);
+    let user2 = new Person('Serhii',36);
+    let userYoung = new Person('Valera',17);
+    Person.prototype.userInfo = function (){
         let fullInfo = {
             name: this.name,
             age: this.age,
@@ -26,19 +26,23 @@
     let auto = new Car('Ford','Transit',2003,'white');
     let auto2 = new Car('Mitsubishi','Lancer',2008,'black');
     Car.prototype.setUser = function (user){
-        if(user.age<18) console.log('Permission denied')
+        if(user.age<18) {
+            console.log('Permission denied');
+            this.user = null;
+        }
         else {
             this.user = user
         }
     }
     Car.prototype.showInfo = function (){
         console.log(
-            'Owner '+ user.userInfo().name +
-            this.brand+this.model+this.age
+            {'Owner':this.user ,
+            'Car':this}
         )
     }
     auto.setUser(user)
-    console.log(auto)
+    auto2.setUser(userYoung)
     auto.showInfo()
+    auto2.showInfo()
 
 })()
