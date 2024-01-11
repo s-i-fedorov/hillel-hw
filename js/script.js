1f7180f219a821b0ab40be6c940b1bbac6777ef2
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-01-12 00:29:50 +0200

    start js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 'use strict';
 (function (){
 
-    let typing = [];
+    const CONST = {
+        typing: [],
+    }
+
     function buttonHandler(e){
         console.dir(e.target);
         const field = document.querySelector('.work-field')
@@ -9,17 +12,21 @@
         // console.log(field)
 
         if(e.target.className==='button' || e.target.hasAttribute('data-btn')){
-            typing.push(e.target.innerHTML)
-            field.innerHTML = typing.join('')
-            result.innerHTML = typing.join('')
+            CONST.typing.push(e.target.innerHTML)
+            field.innerHTML = CONST.typing.join('')
+            result.innerHTML = CONST.typing.join('')
         }
         if(e.target.hasAttribute('data-c')) {
-            typing = []
+            CONST.typing = []
             field.innerHTML = '0'
             result.innerHTML = '0'
             // console.log(1)
         }
         }
     document.addEventListener("click", buttonHandler)
+
+    let a = '5+3*2';
+    console.log(a.trim())
+
 
 })()
