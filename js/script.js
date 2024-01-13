51d0837c0caaf262ac82c8cbe0fba32cfdd9996c
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-01-14 00:04:03 +0200

    start js

--- js/script.js
+++ js/script.js
@@ -7,18 +7,23 @@
     }
 
     function buttonHandler(e){
-        console.dir(e.target);
+        console.log(e.target.classList);
         const field = document.querySelector('.work-field')
         const result = document.querySelector('.result')
 
-        if(e.target.className==='button' || e.target.hasAttribute('data-btn')){
-            CONST.counter++
+
+
+        if(e.target.classList.contains('button')){
+            if(e.target.hasAttribute('data-operand')){CONST.counter=0}
             CONST.typing.push(e.target.innerHTML)
             field.innerHTML = CONST.typing.join('')
             result.innerHTML = CONST.typing.join('')
-            if(CONST.counter===3){
-                CONST.typing.push(' ')
-                CONST.counter=0
+            if(e.target.hasAttribute('data-digit')){
+                CONST.counter++
+                if(CONST.counter===3){
+                    CONST.typing.push(' ')
+                    CONST.counter=0
+                }
             }
         }
         if(e.target.hasAttribute('data-c')) {
@@ -27,11 +32,25 @@
             field.innerHTML = '0'
             result.innerHTML = '0'
         }
+        if(e.target.hasAttribute('data-equal')){
+            let a = CONST.typing.join('');
+            console.log(a)
+            field.innerHTML = eval(a);
+            result.innerHTML = eval(a);
+        }
         }
     document.addEventListener("click", buttonHandler)
 
-    let a = '5+3*2';
-    console.log(a.trim())
+    let a = 18;
+    let b = 3;
+    let c = '+';
+    let d = a+c+b;
+    let z = new Date()
+    let fullDate = z.getDate()+'.'+z.getMonth()+1+'.'+z.getFullYear()+' '+z.getHours()+':'+z.getMinutes()
+    console.log(fullDate)
+    // console.log(eval(d))
+    // console.log(parseInt('8+3',10))
+    // console.log(a.trim())
 
 
 })()
