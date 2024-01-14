17f1ceb40512dd857c5b42fed286bfbac23c474a
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-01-15 00:37:09 +0200

    hw_57 in process

--- js/script.js
+++ js/script.js
@@ -1,45 +1,61 @@
 'use strict';
-(function (){
-    function Student(name,surname, yearBirth, marks, visiting){
+(function () {
+    function Student(name, surname, yearBirth, lessonsCount = 25) {
+        const grades = {
+            min: 0,
+            max: 100
+        }
         this.name = name;
         this.surname = surname;
         this.yearBirth = yearBirth;
-        this.marks = marks;
-        this.visiting = Array(25).fill(null)
-    }
+        this.grades = new Array(lessonsCount);
+        this.visiting = new Array(lessonsCount);
+        this.currentLesson = 0;
 
-    Student.prototype.age = function () {
-        let today = new Date().getFullYear();
-        return today - this.yearBirth
+        Student.prototype.age = function () {
+            let today = new Date().getFullYear();
+            return today - this.yearBirth
         }
-    Student.prototype.summary = function() {
-        let avrMark = this.marks.reduce((acc, i) => acc + i, 0) / this.marks.length;
-        let avrVisiting = this.visiting.reduce((acc, i) => acc + i, 0) / this.visiting.length;
-        if(avrMark>90 && avrVisiting>0.9) {
-            return 'Молодець!'
-        } else if(avrMark<90 && avrVisiting<0.9) {
-            return 'Редиска!'
+        Student.prototype.summary = function () {
+            let avrMark = this.marks.reduce((acc, i) => acc + i, 0) / this.marks.length;
+            let avrVisiting = this.visiting.reduce((acc, i) => acc + i, 0) / this.visiting.length;
+            if (avrMark > 90 && avrVisiting > 0.9) {
+                return 'Молодець!'
+            } else if (avrMark < 90 && avrVisiting < 0.9) {
+                return 'Редиска!'
+            } else return 'Добре, але можна краще!'
         }
-        else return 'Добре, але можна краще!'
+
+        Student.prototype.checkVisiting = function (lessonVisited = true){
+            if(this.visiting>=24) return console.log(`Lessons can't be more than 25`)
+            this.visiting[this.currentLesson] = lessonVisited;
+            this.currentLesson++
         }
-    Student.prototype.absent = function (){
-        let i = this.visiting.findIndex(item=>item===null);
-        if (i===-1) return;
-        this.visiting[i]=0;
-    }
-    Student.prototype.present = function (){
-        let i = this.visiting.findIndex(item=>item===null);
-        if (i===-1) return;
-        this.visiting[i]=1;
-    }
-    Student.prototype.allInfo = function (){
-        const allInfo = {
-            user: this.name,
-            age: this.age(),
-            visiting: this.visiting,
-            summary: this.summary(),
+
+        Student.prototype.absent = function () {
+            this.checkVisiting(false)
         }
-        console.log(allInfo)
+        Student.prototype.present = function () {
+            this.checkVisiting()
+        }
+
+        Student.prototype.setGrade = function (grade){
+            if(typeof grade !== 'number') throw new Error('Grade is not a number')
+            if(grade < grade.min || grade > grade.max) throw new Error('Invalid value');
+            const currentLessonIndex = this.currentLesson - 1;
+            if(!this.visiting[currentLessonIndex]) throw new Error('Student has not been on the lesson')
+            this.grades[currentLessonIndex] = grade;
+        }
+
+        Student.prototype.allInfo = function () {
+            const allInfo = {
+                user: this.name,
+                age: this.age(),
+                visiting: this.visiting,
+                summary: this.summary(),
+            }
+            console.log(allInfo)
+        }
     }
 
 //---------create different users
