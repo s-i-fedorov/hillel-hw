760cf06e46c83bfad4826794ffac3a6a68e31a45
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-01-16 00:08:01 +0200

    hw_57 before create Object

--- js/script.js
+++ js/script.js
@@ -13,11 +13,13 @@
         this.currentLesson = 0;
 
         Student.prototype.age = function () {
+            if(typeof this.yearBirth !== 'number') throw new Error('Birth year should be a number')
             let today = new Date().getFullYear();
+            if((today-this.yearBirth)<14) throw new Error('This student is too young')
             return today - this.yearBirth
         }
         Student.prototype.summary = function () {
-            let avrMark = this.marks.reduce((acc, i) => acc + i, 0) / this.marks.length;
+            let avrMark = this.grades.reduce((acc, i) => acc + i, 0) / this.grades.length;
             let avrVisiting = this.visiting.reduce((acc, i) => acc + i, 0) / this.visiting.length;
             if (avrMark > 90 && avrVisiting > 0.9) {
                 return 'Молодець!'
@@ -48,36 +50,37 @@
         }
 
         Student.prototype.allInfo = function () {
-            const allInfo = {
+            const allInfoObj = {
                 user: this.name,
                 age: this.age(),
                 visiting: this.visiting,
+                grades: this.grades,
                 summary: this.summary(),
             }
-            console.log(allInfo)
+            console.log(allInfoObj)
         }
     }
 
 //---------create different users
-    let studentVovan = new Student('Vovan','Shaitan',1997,[100,100,80,80,90,100]);
-    let studentSerhii= new Student('Serhii','Fedorov', 1987,[80,100,100,100,100,90,100,80]);
-    let studentVasia= new Student('Vasia','Coco', 2005,[80,10,10,100,100,90,100,80]);
+    let studentVovan = new Student('Vovan','Shaitan',1997);
+    let studentSerhii= new Student('Serhii','Fedorov', 1987);
+    // let studentVasia= new Student('Vasia','Coco', 2015,);
 
     //this loops fill arrays 'visiting'
-
-    for (let i = 0; i<3;i++){
-        studentSerhii.absent()
-    }
-    for (let i = 0; i<23;i++){
-        studentVasia.absent()
-    }
-    for (let i = 0; i<25;i++){
-        studentSerhii.present()
-        studentVovan.present()
-    }
+    //
+    // for (let i = 0; i<3;i++){
+    //     studentSerhii.absent()
+    // }
+    // for (let i = 0; i<23;i++){
+    //     studentVasia.absent()
+    // }
+    // for (let i = 0; i<25;i++){
+    //     studentSerhii.present()
+    //     studentVovan.present()
+    // }
 
     studentSerhii.allInfo()
     studentVovan.allInfo()
-    studentVasia.allInfo()
+    // studentVasia.allInfo()
 
 })()
