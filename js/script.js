2e0fc4c46245f8d256ed0d18592c166212a5b627
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-01-16 00:50:53 +0200

    hw_57 incapsulation in process

--- js/script.js
+++ js/script.js
@@ -5,20 +5,19 @@
             min: 0,
             max: 100
         }
-        this.name = name;
-        this.surname = surname;
-        this.yearBirth = yearBirth;
-        this.grades = new Array(lessonsCount);
-        this.visiting = new Array(lessonsCount);
-        this.currentLesson = 0;
-
-        Student.prototype.age = function () {
+        let currentLesson = 0;
+        function age() {
             if(typeof this.yearBirth !== 'number') throw new Error('Birth year should be a number')
             let today = new Date().getFullYear();
             if((today-this.yearBirth)<14) throw new Error('This student is too young')
             return today - this.yearBirth
         }
-        Student.prototype.summary = function () {
+        function checkVisiting (lessonVisited = true){
+            if(this.visiting>=24) return console.log(`Lessons can't be more than 25`)
+            this.visiting[currentLesson] = lessonVisited;
+            currentLesson++
+        }
+        function summary () {
             let avrMark = this.grades.reduce((acc, i) => acc + i, 0) / this.grades.length;
             let avrVisiting = this.visiting.reduce((acc, i) => acc + i, 0) / this.visiting.length;
             if (avrMark > 90 && avrVisiting > 0.9) {
@@ -27,38 +26,44 @@
                 return 'Редиска!'
             } else return 'Добре, але можна краще!'
         }
-
-        Student.prototype.checkVisiting = function (lessonVisited = true){
-            if(this.visiting>=24) return console.log(`Lessons can't be more than 25`)
-            this.visiting[this.currentLesson] = lessonVisited;
-            this.currentLesson++
-        }
-
-        Student.prototype.absent = function () {
-            this.checkVisiting(false)
-        }
-        Student.prototype.present = function () {
-            this.checkVisiting()
-        }
-
-        Student.prototype.setGrade = function (grade){
+        function setGrade(grade){
             if(typeof grade !== 'number') throw new Error('Grade is not a number')
             if(grade < grade.min || grade > grade.max) throw new Error('Invalid value');
-            const currentLessonIndex = this.currentLesson - 1;
+            const currentLessonIndex = currentLesson - 1;
             if(!this.visiting[currentLessonIndex]) throw new Error('Student has not been on the lesson')
             this.grades[currentLessonIndex] = grade;
         }
+        return {
+            name,
+            surname,
+            yearBirth,
+            grades: new Array(lessonsCount),
+            visiting: new Array(lessonsCount),
+            age() {
+                age.call(this)
+            },
+            absent() {
+                checkVisiting.call(this,false)
+            },
+            present() {
+                checkVisiting.call(this)
+            },
+            setGrade(grade){
+                setGrade.call(this, grade)
+            },
 
-        Student.prototype.allInfo = function () {
-            const allInfoObj = {
-                user: this.name,
-                age: this.age(),
-                visiting: this.visiting,
-                grades: this.grades,
-                summary: this.summary(),
-            }
-            console.log(allInfoObj)
         }
+
+        // Student.prototype.allInfo = function () {
+        //     const allInfoObj = {
+        //         user: this.name,
+        //         age: this.age(),
+        //         visiting: this.visiting,
+        //         grades: this.grades,
+        //         summary: this.summary(),
+        //     }
+        //     console.log(allInfoObj)
+        // }
     }
 
 //---------create different users
@@ -79,8 +84,20 @@
     //     studentVovan.present()
     // }
 
-    studentSerhii.allInfo()
-    studentVovan.allInfo()
+    // studentSerhii.allInfo()
+    // studentVovan.allInfo()
     // studentVasia.allInfo()
+    studentSerhii.absent()
+    studentSerhii.present()
+    studentSerhii.setGrade(50)
+    studentSerhii.absent()
+    studentSerhii.present()
+    studentSerhii.setGrade(50)
+    studentSerhii.present()
+    studentSerhii.setGrade(50)
+    studentSerhii.present()
+    // studentSerhii.setGrade(50)
+
+    console.table(studentSerhii)
 
 })()
