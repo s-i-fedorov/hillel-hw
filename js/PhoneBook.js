7a54100118d8252b5fb996277f06bba5dee209d8
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-02-04 00:55:08 +0200

    hw_59 duration and history

--- js/PhoneBook.js
+++ js/PhoneBook.js
@@ -5,18 +5,33 @@ class PhoneBook {
 
   #usersListSelector = null;
 
-  #removeSelector = null;
+  #removeAttr = null;
 
-  #callSelector = null;
+  #callAttr = null;
 
-  constructor(users, userListSelector) {
+  #modal = null;
+
+  #callControllerInstance = null;
+
+  constructor(users, userListSelector, removeSelector = null, callSelector = null, modalSelector = null) {
     // Validate users
     // add users to contacts
-    if (!PhoneBook.validateSelector(userListSelector)) throw new Error('This selector is not exist');
+    if (!PhoneBook.validateSelector(modalSelector)) throw new Error('modalSelector selector is not exist');
+    this.#modal = new bootstrap.Modal(modalSelector);
+
+    if (!PhoneBook.validateSelector(userListSelector)) throw new Error('userListSelector selector is not exist');
     this.#usersListSelector = document.querySelector(userListSelector);
+    if (!removeSelector) throw new Error('removeSelector is not defined');
+    this.#removeAttr = removeSelector;
+    if (!callSelector) throw new Error('callSelector is not defined');
+    this.#callAttr = callSelector;
+    this.#callControllerInstance = new CallController();
+
     users.forEach((user) => {
       this.addContact(user);
     });
+
+    this.#setEvents();
   }
 
   addContact(user) {
@@ -29,9 +44,9 @@ class PhoneBook {
     this.renderContact(this.#contacts.at(-1));
   }
 
-  call(contactId) {
-    // find contact in this.#contacts and make a call
-  }
+  // call(contactId) {
+  //   // find contact in this.#contacts and make a call
+  // }
 
   removeContact(contactId) {
     // will remove contact from this.#contacts
@@ -43,7 +58,47 @@ class PhoneBook {
 
   #setEvents() {
     // Will add event listeners to contact book
+    Call.addChangeStatusListener(this.#changeCallStatusHandler);
+    document.querySelector('[data-end-call]').addEventListener('click', this.#endCallHandler);
+    this.#usersListSelector.addEventListener('click', this.#removeHandler);
+    this.#usersListSelector.addEventListener('click', this.#callHandler);
   }
+
+  #removeHandler = ({ target }) => {
+    const currentClickedBtn = target.closest(`[${this.#removeAttr}]`);
+    if (!currentClickedBtn) return;
+    const contactTemplate = currentClickedBtn.closest('[data-user-id]');
+    const contactId = +contactTemplate.getAttribute('data-user-id');
+    const index = this.#contacts.findIndex((i) => i.id === contactId);
+    this.#contacts.splice(index, 1);
+    contactTemplate.remove();
+  };
+
+  #endCallHandler = () => {
+    this.#callControllerInstance.endCallByCaller();
+  };
+
+  #callHandler = ({ target }) => {
+    const currentClickedBtn = target.closest(`[${this.#callAttr}]`);
+    if (!currentClickedBtn) return;
+    const contactTemplate = currentClickedBtn.closest('[data-user-id]');
+    const contactId = +contactTemplate.getAttribute('data-user-id');
+    const index = this.#contacts.findIndex((i) => i.id === contactId);
+    const currentContact = this.#contacts[index];
+    document.querySelector('[data-abonent-name]').innerHTML = currentContact.name;
+
+    this.#callControllerInstance.startCall(this.#contacts[index].phone);
+    this.#modal.show();
+
+    // console.log(this.#callControllerInstance.callHistory);
+  };
+
+  #changeCallStatusHandler = (callStatus) => {
+    document.querySelector('[data-call-body]').innerHTML = callStatus;
+    if (callStatus === Call.CALL_STATUSES.disconnect || callStatus === Call.CALL_STATUSES.rejected) {
+      this.#modal.hide();
+    }
+  };
 
   // your methods
   // All event handlers should be a separate private methods
@@ -80,5 +135,11 @@ class PhoneBook {
   }
 }
 
-const phoneBook = new PhoneBook(users, '.contacts__list ul');
+const phoneBook = new PhoneBook(
+  users,
+  '.contacts__list ul',
+  'data-remove',
+  'data-call',
+  '#staticBackdrop',
+);
 console.log(phoneBook);
