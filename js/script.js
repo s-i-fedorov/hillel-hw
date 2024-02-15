1e59117d2356d5faa257ee11104b815947eea4e6
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-02-15 19:12:26 +0200

    hw_67_completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
-'use strict';
-(function (){
+(function () {
+  const data1 = {
+    x: 5,
+    y: 8,
+  };
+  const data2 = {
+    x: 10,
+    y: 20,
+  };
+  const data3 = {
+    x: 15,
+    y: 40,
+  };
 
-})()
+  const cache = new WeakMap();
+
+  function cylinderVolume(data) {
+    return (data.x / 2) * (data.x / 2) * 3.14 * data.y;
+  }
+
+  function cacheComplexCalculations(data, callback) {
+    if (cache.has(data)) {
+      console.log('here');
+      const result = cache.get(data);
+      return { data, result };
+    }
+    const result = callback(data);
+    cache.set(data, result);
+    return { data, result };
+  }
+
+  console.log(cacheComplexCalculations(data1, cylinderVolume));
+  console.log(cacheComplexCalculations(data2, cylinderVolume));
+  console.log(cacheComplexCalculations(data1, cylinderVolume));
+  console.log(cacheComplexCalculations(data3, cylinderVolume));
+  console.log(cacheComplexCalculations(data2, cylinderVolume));
+  console.log(cache);
+}());
