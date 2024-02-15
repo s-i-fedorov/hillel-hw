47844a327efdf944fa9046c1c7b5424b9e3e95f5
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-02-15 23:10:59 +0200

    hw_68_completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-'use strict';
-(function (){
+(function () {
+  const data1 = {
+    name: 'vasia',
+    surname: 'petrov',
+  };
+  const data2 = {
+    name: 'vladimir',
+    surname: 'komarov',
+  };
+  const data3 = {
+    name: 'taras',
+    surname: 'bulba',
+  };
 
-})()
+  const newSet = new WeakSet();
+  function trackObjects(object) {
+    if (!newSet.has(object)) {
+      newSet.add(object);
+      return false;
+    }
+    if (newSet.has(object)) return true;
+  }
+  trackObjects(data1);
+  trackObjects(data2);
+  console.log(trackObjects(data1));
+  console.log(newSet.has(data3));
+}());
