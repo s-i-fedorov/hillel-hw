f9f14e4e9192040620bfb3b3f28a2f9a351c0b36
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-02-19 01:03:09 +0200

    hw_69 in process

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-'use strict';
-(function (){
+;(function () {
+    async function getPosts(id) {
+        if (!(typeof id === 'number') || !(id > 0) || !(id <= 100))
+            throw new Error('You entered invalid "Id"')
+        const req = await fetch(
+            `https://jsonplaceholder.typicode.com/posts?id=${id}`
+        )
+        const data = await req.json()
+        if (data.length > 1) throw new Error('Something went wrong')
+        dataObj = data[0]
+        return dataObj
+        // console.log(dataObj.body)
+    }
 
+    // console.dir(getPosts(5))
+    const result = getPosts(5)
+    result.then((response) => {
+        console.log(response)
+    })
 })()
